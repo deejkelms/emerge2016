48756045a96084242f08d9d5517e6c06557cd63f
deejkelms <dkelmanson@gmail.com>
2016-04-16 19:43:55 +0000

    session for both user and bar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,14 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   include SessionsHelper
+  helper_method :current_artist
+  helper_method :current_fan
+
+  def current_bar
+    @current_fan ||= Bar.find_by(id: session[:bar_id])
+  end
+
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
 end
