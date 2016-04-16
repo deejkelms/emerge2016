ec9c6399d1c9b9a94521d533cbcc2ae9836a83dc
Jhlechia Qureshi <jhlechiae@gmail.com>
2016-04-16 14:48:16 -0400

    user login

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,4 +1,11 @@
 module SessionsHelper
+  #Logs in user
   def log_in(user)
     session[:user_id] = user_id
+  end
+
+  # Returns the current logged-in user (if any).
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
 end
